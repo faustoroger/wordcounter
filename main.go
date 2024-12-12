516e37513f828a3e0499b4072d5ebc5bb5be219d
Fausto Roger <9322200+faustoroger@users.noreply.github.com>
2024-12-12 06:53:19 -0300

    :sparkles: feat: Added flag option

--- main.go
+++ main.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	fmt.Println(count(os.Stdin))
+	lines := flag.Bool("l", false, "Count lines")
+	flag.Parse()
+
+	if *lines {
+		fmt.Println("The statement has", count(os.Stdin, *lines), "number of lines.")
+	} else {
+		fmt.Println("The statement has", count(os.Stdin, *lines), "number of words.")
+	}
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
 	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanWords)
+
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
+
 	wc := 0
 	for scanner.Scan() {
 		wc++
