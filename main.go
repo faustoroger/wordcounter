02f7f771b3b9a9066d5728a3d1334cf102b344d3
Fausto Roger <hixian@gmail.com>
2024-12-18 02:35:32 -0300

    :sparkles: feat: Added option to open file

--- main.go
+++ main.go
@@ -13,12 +13,44 @@ func main() {
 	countRunes := flag.Bool("r", false, "Count runes")
 	flag.Parse()
 
-	if !*countLines && !*countRunes {
-		fmt.Println("The content has", count(os.Stdin, *countLines, *countRunes), "number of words.")
-	} else if *countRunes {
-		fmt.Println("The content has", count(os.Stdin, *countLines, *countRunes), "number of runes.")
-	} else {
-		fmt.Println("The content has", count(os.Stdin, *countLines, *countRunes), "number of lines.")
+	if *countLines && *countRunes {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if flag.NArg() == 0 {
+		if *countLines {
+			fmt.Println("The content has", count(os.Stdin, *countLines, *countRunes), "number of lines.")
+		} else if *countRunes {
+			fmt.Println("The content has", count(os.Stdin, *countLines, *countRunes), "number of runes.")
+		} else {
+			fmt.Println("The content has", count(os.Stdin, *countLines, *countRunes), "number of words.")
+		}
+	}
+
+	var (
+		f   *os.File
+		err error
+	)
+
+	if flag.NArg() == 1 {
+		f, err = os.Open(flag.Arg(0))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Cannot open file %s: %s\n", flag.Arg(0), err)
+			os.Exit(1)
+		}
+	}
+
+	defer f.Close()
+
+	if f != nil {
+		if *countLines && !*countRunes {
+			fmt.Println("The content has", count(f, *countLines, *countRunes), "number of lines.")
+		} else if *countRunes && !*countLines {
+			fmt.Println("The content has", count(f, *countLines, *countRunes), "number of runes.")
+		} else {
+			fmt.Println("The content has", count(f, *countLines, *countRunes), "number of words.")
+		}
 	}
 }
 
@@ -27,8 +59,10 @@ func count(r io.Reader, countLines bool, countRunes bool) int {
 
 	if !countLines && !countRunes {
 		scanner.Split(bufio.ScanWords)
-	} else if countRunes {
+	} else if !countLines && countRunes {
 		scanner.Split(bufio.ScanRunes)
+	} else if countLines && !countRunes {
+		scanner.Split(bufio.ScanLines)
 	}
 
 	wc := 0
